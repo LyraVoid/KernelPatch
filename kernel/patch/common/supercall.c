fa62c5127225c8ea3b9fb5723d45604a42d90ed6
YAIU <2146142532@qq.com>
2026-05-10 17:08:13 +0800

    123

--- kernel/patch/common/supercall.c
+++ kernel/patch/common/supercall.c
@@ -512,35 +512,33 @@ int is_trusted_manager_uid(uid_t uid)
 
 static void before(hook_fargs6_t *args, void *udata)
 {
-    int uid = current_uid();
-
-    int is_trusted_caller = 0;
-    int is_authed = 0;
-    if (has_preset_superkey()) {
-        const char *__user key_user = (const char *__user)syscall_argn(args, 0);
-        
-        char key[MAX_KEY_LEN];
-        long len = compat_strncpy_from_user(key, key_user, MAX_KEY_LEN);
-        if (len <= 0) return;
-        is_authed = !auth_superkey(key);
-        is_trusted_caller = is_authed;
-    }
-    if (is_trusted_manager_uid(uid)) {
-        is_trusted_caller = 1;
-        is_authed = 1;
-    } else if (is_su_allow_uid(uid)) {
-        is_trusted_caller = 1;
-    }
-
-    if (!is_trusted_caller) return;
-
+    const char *__user ukey = (const char *__user)syscall_argn(args, 0);
     long ver_xx_cmd = (long)syscall_argn(args, 1);
-    long cmd = ver_xx_cmd & 0xFFFF;
-    if (cmd < SUPERCALL_HELLO || cmd > SUPERCALL_MAX) return;
 
     // todo: from 0.10.5
     // uint32_t ver = (ver_xx_cmd & 0xFFFFFFFF00000000ul) >> 32;
     // long xx = (ver_xx_cmd & 0xFFFF0000) >> 16;
+
+    long cmd = ver_xx_cmd & 0xFFFF;
+    if (cmd < SUPERCALL_HELLO || cmd > SUPERCALL_MAX) return;
+
+    char key[MAX_KEY_LEN];
+    long len = compat_strncpy_from_user(key, ukey, MAX_KEY_LEN);
+    if (len <= 0) return;
+
+    int is_key_auth = 0;
+    int is_trusted_manager = 0;
+    is_trusted_manager = is_trusted_manager_uid(current_uid());
+    if (is_trusted_manager) {
+        is_key_auth = 1;
+    }
+    is_key_auth = 1;
+    if (!strcmp("su", key)) {
+        uid_t uid = current_uid();
+        if (!is_su_allow_uid(uid) && !is_trusted_manager) return;
+    } else {
+        if (!is_trusted_manager) return;
+    }
 
     long a1 = (long)syscall_argn(args, 2);
     long a2 = (long)syscall_argn(args, 3);
@@ -548,7 +546,7 @@ static void before(hook_fargs6_t *args, void *udata)
     long a4 = (long)syscall_argn(args, 5);
 
     args->skip_origin = 1;
-    args->ret = supercall(is_authed, cmd, a1, a2, a3, a4);
+    args->ret = supercall(is_key_auth, cmd, a1, a2, a3, a4);
 }
 
 int supercall_install()
