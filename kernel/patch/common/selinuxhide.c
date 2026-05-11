baebca36c158f2c6fc9d5354ebf5464e3cfcec4a
YAIU <2146142532@qq.com>
2026-05-12 00:39:30 +0800

    xf

--- kernel/patch/common/selinuxhide.c
+++ kernel/patch/common/selinuxhide.c
@@ -60,6 +60,10 @@ ssize_t hk_sel_write_access(struct file *file, char *buf, size_t size){
         pr_info("selinux-hide: found LSPosed\n");
         return -22;
     }
+    if(strstr(buf,"adb_data_file")){
+        pr_info("selinux-hide: found ZygiskNext\n");
+        return -22;
+    }
     pr_info("selinux-hide access uid: %d buf: %s\n",uid,buf);
     // 默认允许
     return back_sel_write_access(file,buf,size);
